52cbce6cde47c569d292a2b750fdc15a0f66e96d
dongjin <dongjin@DESKTOP-9394P05>
2022-11-18 23:52:33 +0900

    programmers

---
+++ Schedular/src/com/codeFactory/TEST_20221118.java
@@ -0,0 +1,12 @@
+package com.codeFactory;
+
+public class TEST_20221118 {
+
+	class Solution {
+	    public int solution(int n, int[] lost, int[] reserve) {
+	        int answer = 0;
+	        return answer;
+	    }
+	}
+	
+}
